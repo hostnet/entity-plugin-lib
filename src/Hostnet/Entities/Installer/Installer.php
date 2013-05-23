c5fc14c91d2c5acde37b770077a005ab95fc50d2
Nico Schoenmaker <nschoenmaker@hostnet.nl>
2013-05-23 11:21:40 +0200

    First test installer

---
+++ src/Hostnet/Entities/Installer/Installer.php
@@ -0,0 +1,19 @@
+<?php
+namespace Hostnet\Entities\Installer;
+
+use Composer\Package\PackageInterface;
+
+use Composer\Installer\LibraryInstaller;
+
+/**
+ * Custom installer to generate the various traits and interfaces for that package
+ * @author Nico Schoenmaker <nschoenmaker@hostnet.nl>
+ */
+class Installer extends LibraryInstaller
+{
+  protected function installBinaries(PackageInterface $package)
+  {
+    parent::installBinaries($package);
+    $this->io->write("Hello, world");
+  }
+}
