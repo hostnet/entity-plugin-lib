3e6c18793ccc53e384435faf9b50c567ddceb06a
Nico Schoenmaker <nschoenmaker@hostnet.nl>
2013-11-27 10:39:56 +0100

    Converted the installer into a plugin

---
+++ src/Hostnet/Entities/Installer/Plugin.php
@@ -0,0 +1,33 @@
+<?php
+namespace Hostnet\Entities\Installer;
+
+use Composer\Composer;
+use Composer\EventDispatcher\EventSubscriberInterface;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+use Composer\Script\ScriptEvents;
+
+class Plugin implements PluginInterface, EventSubscriberInterface
+{
+  private $installer;
+
+  public function activate(Composer $composer, IOInterface $io)
+  {
+    $this->installer = new Installer($io, $composer);
+    $composer->getInstallationManager()->addInstaller($this->installer);
+  }
+
+  public static function getSubscribedEvents()
+  {
+    return array(ScriptEvents::POST_AUTOLOAD_DUMP => array('onPostAutoloadDump', 0));
+  }
+
+  /**
+   * Gets called on the POST_AUTOLOAD_DUMP event
+   */
+  public function onPostAutoloadDump()
+  {
+    $this->installer->postAutoloadDump();
+  }
+
+}
