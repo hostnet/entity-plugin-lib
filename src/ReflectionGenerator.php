c988b431dde4bbc1a7d43d3a8c83ed42b89736a2
Hidde Boomsma <hidde@hostnet.nl>
2014-05-12 18:29:33 +0200

    and the dir fix

--- src/ReflectionGenerator.php
+++ src/ReflectionGenerator.php
@@ -112,7 +112,7 @@ class ReflectionGenerator
         if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
             include __DIR__ . '/../vendor/autoload.php';
         } else {
-            include __DIR__ . '/../../autoload.php';
+            include __DIR__ . '/../../../autoload.php';
         }
         // @codeCoverageIgnoreEnd
 
