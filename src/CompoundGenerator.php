d5b1a7b4e56fd891a1997e185935f2896c3838ac
Yannick de Lange <ydelange@hostnet.nl>
2014-07-24 14:35:17 +0200

    Did a small cleanup of template by pre-filtering

--- src/CompoundGenerator.php
+++ src/CompoundGenerator.php
@@ -144,7 +144,7 @@ class CompoundGenerator
             array(
                 'class_name' => $short_name,
                 'namespace' => $generated_namespace,
-                'use_statements' => $traits
+                'use_statements' => array_filter($traits, function (PackageClass $stmt) { return $stmt->isTrait();})
             )
         );
 
